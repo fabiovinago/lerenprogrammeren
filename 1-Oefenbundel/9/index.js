27d433ea642eaa50f33804fba2e4d3577690adc0
fabiovinago <fabiovinago.be@gmail.com>
2024-09-26 22:30:25 +0200

    aanpassing oefening9

--- 1-Oefenbundel/9/index.js
+++ 1-Oefenbundel/9/index.js
@@ -6,8 +6,8 @@ te printen die deelbaar zijn door 3.
 let getal1 = 1;
 
 for(getal1;getal1<=30;getal1++) {
-    if (getal1%3===0) {
+    if (getal1 % 3 === 0) {
         console.log(getal1);
     }
-
+}
 
