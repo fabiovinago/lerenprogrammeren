9c1bb8c7b67c8a5e558cf6b849a79aa8c353f098
fabiovinago <fabiovinago.be@gmail.com>
2024-09-27 10:09:20 +0200

    Aanpassing herhaling oefenbundel

---
+++ 2-Herhaling oefenbundel/3/index.js
@@ -0,0 +1,10 @@
+
+/*Print even getallen: Gebruik een for-loop om de even getallen van 0 tot 20 te printen.
+ */
+
+let getal1 = 0;
+
+for(getal1;getal1<=20;getal1++){
+    if(getal1%2===0)
+        console.log(getal1)
+}
---
+++ 2-Herhaling oefenbundel/4/index.js
@@ -0,0 +1,11 @@
+
+/*Print oneven getallen: Gebruik een for-loop om de oneven getallen van 1 tot 19 te
+printen.
+ */
+
+let getal1 = 1;
+
+for(getal1;getal1<=19;getal1++){
+    if(getal1%2!==0)
+        console.log(getal1)
+}
