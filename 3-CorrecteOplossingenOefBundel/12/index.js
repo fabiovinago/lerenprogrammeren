d3cf0be61d9baa315d32915c8de3ee799c86da5e
fabiovinago <fabiovinago.be@gmail.com>
2024-09-28 10:56:33 +0200

    Oplossingen oefenbundel 1tot15

---
+++ 3-CorrecteOplossingenOefBundel/12/index.js
@@ -0,0 +1,13 @@
+
+/*Tel af van een gegeven getal: Vraag een getal aan de gebruiker en tel af naar 0 met een
+for-loop.
+ */
+
+
+let getal = parseInt(prompt("Geef een getal in"));
+let getal1 = getal;
+
+for(getal;getal>=0;getal--){
+    console.log(getal)
+
+}
---
+++ 3-CorrecteOplossingenOefBundel/15/index.js
@@ -0,0 +1,20 @@
+
+/*Print de veelvouden van een gegeven getal: Vraag een getal aan de gebruiker en print
+de veelvouden van dat getal tot 100.
+ */
+
+
+
+let nummer = parseInt(prompt("Voer een getal in"));
+
+let getal1 = 1;
+
+for(getal1;getal1*nummer<=100;getal1++){
+    console.log(nummer*getal1);
+}
+
+
+
+
+
+
